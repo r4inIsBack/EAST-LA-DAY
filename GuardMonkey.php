acdfe488e2ff20c93c186d893e48ea15abd6792a
WAKA*FEST <246561003+r4inIsBack@users.noreply.github.com>
2026-02-26 02:19:25 -0600

    Create GuardMonkey.php

---
+++ GuardMonkey.php
@@ -0,0 +1,121 @@
+#EXTM3U
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-1
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://186.235.119.2:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-2
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://177.190.200.194:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-3
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.84:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-4
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.93:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-5
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.89:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-6
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://177.92.108.26:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-7
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.88:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-8
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.87:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-9
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.90:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-10
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.81:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-11
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.92:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-12
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.85:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-13
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.86:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-14
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.82:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-15
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.95:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-16
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.94:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-17
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.227.42:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-18
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.91:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-19
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.83:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-20
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://200.183.142.80:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-21
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://201.48.125.62:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-22
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://177.190.201.188:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-23
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://187.9.101.194:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
+#EXTINF:-1 group-title="ClaroTV+ BR", A&E HD OPT-24
+#KODIPROP:inputstream.adaptive.manifest_type=mpd
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"6cf551b65ef233dda7ef03cee8bf4408":"ee3debba9d8acbb31e622edb62ede595"}
+http://45.188.19.86:9999/https://getcdn.nowonline.com.br/Content/Channel/SPOAEMHD/dsc1/manifest.mpd?420GWahWah_R4IN&DON'TSTEAL_OR_RESELL_YOUWILLBEBLOCKED
