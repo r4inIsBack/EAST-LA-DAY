d92ef0af748f62cbd42940909423f61cacfa34b7
WAKA*FEST <246561003+r4inIsBack@users.noreply.github.com>
2026-02-27 09:22:27 -0600

    Update and rename m3ukodis.m3u to m3ukodi.php

---
+++ m3ukodi.php
@@ -0,0 +1,48 @@
+#EXTM3U
+# Modo: Individual
+# Canal: PLAYLIST
+# Grupo: LIL PEEP
+# Categoria: VIDEOS MUSICALES
+# Subgrupo: MUSICA
+#
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=hls
+#KODIPROP:inputstream.adaptive.manifest_update_parameter=full
+#KODIPROP:inputstream.adaptive.play_timeshift_buffer
+#EXTVLCOPT:http-reconnect
+#EXTVLCOPT:http-continuous
+#EXTVLCOPT:clock-synchro=0
+#EXTVLCOPT:clock-jitter=0
+#EXTVLCOPT:no-audio-desync
+#EXTVLCOPT:avcodec-hw=auto
+#EXTVLCOPT:avcodec-fast
+#EXTINF:-1 tvg-id="osPq9Yb8xm8" tvg-logo="https://i.ytimg.com/vi/osPq9Yb8xm8/hqdefault.jpg" group-title="LIL PEEP" group-logo="https://tinyurl.com/logom3ukodi",[LIL PEEP] LIL PEEP - NUTS
+https://m3ukodiiptv.alwaysdata.net/root/youtube/yt_player.php?video_id=osPq9Yb8xm8
+
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=hls
+#KODIPROP:inputstream.adaptive.manifest_update_parameter=full
+#KODIPROP:inputstream.adaptive.play_timeshift_buffer
+#EXTVLCOPT:http-reconnect
+#EXTVLCOPT:http-continuous
+#EXTVLCOPT:clock-synchro=0
+#EXTVLCOPT:clock-jitter=0
+#EXTVLCOPT:no-audio-desync
+#EXTVLCOPT:avcodec-hw=auto
+#EXTVLCOPT:avcodec-fast
+#EXTINF:-1 tvg-id="E7sP6t1QyrI" tvg-logo="https://i.ytimg.com/vi/E7sP6t1QyrI/hqdefault.jpg" group-title="LIL PEEP" group-logo="https://tinyurl.com/logom3ukodi",[LIL PEEP] LIL PEEP X LIL TRACY - WITCHBLADES
+https://m3ukodiiptv.alwaysdata.net/root/youtube/yt_player.php?video_id=E7sP6t1QyrI
+
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=hls
+#KODIPROP:inputstream.adaptive.manifest_update_parameter=full
+#KODIPROP:inputstream.adaptive.play_timeshift_buffer
+#EXTVLCOPT:http-reconnect
+#EXTVLCOPT:http-continuous
+#EXTVLCOPT:clock-synchro=0
+#EXTVLCOPT:clock-jitter=0
+#EXTVLCOPT:no-audio-desync
+#EXTVLCOPT:avcodec-hw=auto
+#EXTVLCOPT:avcodec-fast
+#EXTINF:-1 tvg-id="fudsUhWAG_o" tvg-logo="https://i.ytimg.com/vi/fudsUhWAG_o/hqdefault.jpg" group-title="LIL PEEP" group-logo="https://tinyurl.com/logom3ukodi",[LIL PEEP] LIL PEEP W/ YUNG BRUH - WHITE TEE
+https://m3ukodiiptv.alwaysdata.net/root/youtube/yt_player.php?video_id=fudsUhWAG_o
