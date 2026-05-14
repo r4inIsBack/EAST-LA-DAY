8ed187e27dfdaef14cc4026559456136dbdf135a
TAKOxGANG <246561003+r4inIsBack@users.noreply.github.com>
2026-05-14 16:45:56 -0500

    Add Vive TV streaming configuration to vene.ts

---
+++ vene.ts
@@ -0,0 +1,5 @@
+#EXTINF:-1 group-title="InterGo Venezuela", Vive TV
+#KODIPROP:inputstream.adaptive.manifest_type=m3u8
+#KODIPROP:inputstream.adaptive.license_type=clearkey
+#KODIPROP:inputstream.adaptive.license_key={"40caf7243fd61baaa88a451f73f4f990":"2250f7ffe44e1e18c6a7aef092865171"}
+https://ccs-offnet-cdn2.netuno.cdn.moderntv.eu/netuno/stream/VIVE_TV/20-hls/storage-media.m3u8?_cdn_attrs=account%3Dnetuno%2Cresource%3DVIVE_TV_stream_wy&_cdn_session=18957803790&contentId=b9b2cdb3-af63-4ea7-84d4-a3d84ac3cad8&contentType=&drmProvider=cg&drmTypes=widevine&duration=14580&packager=1&start=1778788740&stream=video%3A0&trackType=HD&_cdn_token=3c058ccdfe4fcfc920355a967f21e104a9b3bc6a
